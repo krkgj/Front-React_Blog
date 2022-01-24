ed58ea8102f37cb1ca245e9a33da174d59a23444
Kyeongjin Kim <krkgj0526@naver.com>
2022-01-24 23:37:42 +0900

    initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Container } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
+import Main from "pages/Main";
 function App() {
   return (
     <Container fluid>
-      <div className="App">
-        <Button variant="primary">Primary</Button>{" "}
-        <Button variant="secondary">Secondary</Button>{" "}
-        <Button variant="success">Success</Button>
-        <Button variant="warning">Warning</Button>{" "}
-        <Button variant="danger">Danger</Button>{" "}
-        <Button variant="info">Info</Button>{" "}
-        <Button variant="light">Light</Button>{" "}
-        <Button variant="dark">Dark</Button>{" "}
-        <Button variant="link">Link</Button>
-      </div>
+      <Row>
+        <Col xs={6}>1 of 1</Col>
+        <Col xs={6}>2 of 1</Col>
+        <Col xs={12}>
+          <Main></Main>
+        </Col>
+      </Row>
     </Container>
   );
 }
